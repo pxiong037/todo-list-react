743a39f7c0a25f2d5fef482ffbaba38160ed46c1
Prechar <pxiong037@gmail.com>
2020-08-12 05:16:33 +0000

    Updates

---
+++ precache-manifest.ae39fe5219dcf02a5517aa858f3482f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "236e572350117612eeba9362bbca29fd",
+    "url": "/todo-list-react/index.html"
+  },
+  {
+    "revision": "096582d969a9a8e90660",
+    "url": "/todo-list-react/static/css/main.a570613b.chunk.css"
+  },
+  {
+    "revision": "7fdfad3df2d7dda9d821",
+    "url": "/todo-list-react/static/js/2.fffd499a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-list-react/static/js/2.fffd499a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "096582d969a9a8e90660",
+    "url": "/todo-list-react/static/js/main.4c23f77c.chunk.js"
+  },
+  {
+    "revision": "826642a5112903c57c77",
+    "url": "/todo-list-react/static/js/runtime-main.22aeb5a4.js"
+  }
+]);
